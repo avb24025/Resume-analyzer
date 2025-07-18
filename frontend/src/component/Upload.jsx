9bc161faadbf52a2f65d8d9139802a1735881038
avb24025 <avb24022005@gmail.com>
2025-07-18 14:48:18 +0530

    Final Commit

--- frontend/src/component/Upload.jsx
+++ frontend/src/component/Upload.jsx
@@ -5,7 +5,6 @@ import axios from "axios";
 function Spinner() {
   return (
     <div className="flex justify-center items-center mt-4">
-    <div>API is Free...It Takes Time...</div>
       <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500"></div>
     </div>
   );
